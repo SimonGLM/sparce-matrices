80272f401b9426d2057b725bd8856135f9de0423
Henrik Spielvogel <spielvogel.henrik@gmail.com>
2020-02-26 14:36:05 +0100

    Revert "meaningless test commit"
    This reverts commit 6f19b8bc322ea5c58522cb10b668a159bb959dcf.

--- libsparse.py
+++ libsparse.py
@@ -23,10 +23,6 @@ class sparse(object):
         pass
 
     # TODO: Needs class methods for gaussian elimination
-    def func(x):
-        '''Author: henrik spielvogel'''
-        
-        pass
         
 if __name__ == "__main__":
     pass
